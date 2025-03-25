075a00b336b12f88ee9b68d5629352f0b5c6c1be
YZcontent <tal@yzcontent.com>
2025-03-24 23:44:29 -0700

    Delete api directory

--- api/yodeck-sync.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    return res.status(200).json({ success: true, message: 'Yodeck sync endpoint working' });
-  }
-
-  return res.status(405).json({ success: false, message: 'Method Not Allowed' });
-}
